3e7689f6ad3daabe6f22d92b65259e5eddb68218
jakow <qbol1234@hotmail.com>
2016-01-20 14:48:12 +0000

    RPJplayer rework

--- RPJplayer.py
+++ RPJplayer.py
@@ -8,6 +8,7 @@ __author__ = 'Jakub'
 
 eventlet.monkey_patch(all=True)
 
+
 class RPJPlayer(object):
     """ RPJ wrapper for Mplayer Python wrapper
      Communicates with Mplayer via sockets and so is completely asynchronous """
@@ -124,6 +125,7 @@ class RPJPlayer(object):
 
     def quit(self):
         self.player.quit()
+
     ####### PRIVATE METHODS ##########################3333
 
     def _playback_end_event(self, data):
@@ -150,13 +152,17 @@ class RPJPlayer(object):
             print 'mplayer pipe: ' + data
 
 
-def notifying(func):
-    def notifying_function(*args, **kwargs):
-        return_val = func(*args, **kwargs)
-        args[0].execute_event_hooks()
-        return return_val
+# decorator to be used with RPJplayer to trigger events
+def notify(event_name):
 
-    return notifying_function
+    def notifying(func):
+        def wrapper(*args, **kwargs):
+            print event_name
+            return_val = func(*args, **kwargs)
+            eventlet.spawn(args[0].fire_event, event_name)  # args[0] = self when used in a bound method
+            return return_val
+        return wrapper
+    return notifying
 
 
 class RPJPlayerV2(object):
@@ -169,11 +175,12 @@ class RPJPlayerV2(object):
         self._mplayer.args = ['-really-quiet', '-msglevel', 'global=6']
 
         # set up event handlers (private) and hooks (public)
-        self._event_handlers = {"playback_finish": [],
-                                "playback_skip": [],
-                                "playback_stop": []
-                                }
-        self._event_hooks = []
+        self._mplayer_event_handlers = {"playback_finish": [],
+                                        "playback_skip": [],
+                                        "playback_stop": []
+                                        }
+        # event listeners is a tuple of (event_name, listener)
+        self._event_listeners = []
         self._mplayer.stdout.connect(self._playback_end_event_handler)
 
         # now set up state
@@ -188,9 +195,12 @@ class RPJPlayerV2(object):
         self._mplayer.spawn()
         self._mplayer.volume = self._vol
 
+    def __del__(self):
+        self._mplayer.quit()
+
     def state(self):
         return json.dumps({
-            "is_playing": self._is_playing, # will change
+            "is_playing": self._is_playing,  # will change
             "now_playing": self._now_playing,
             "position": self._mplayer.time_pos,
             "volume": self._vol,
@@ -200,7 +210,7 @@ class RPJPlayerV2(object):
         })
 
     # load file for playback and play immediately
-    @notifying
+    @notify('state_changed')
     def load(self, song):
         if isinstance(song, dict):  # check if a song dict is given
             path = self.workingDirectory + song['id'] + '.mp3'
@@ -212,25 +222,36 @@ class RPJPlayerV2(object):
         if self._paused:  # unpause
             self.pause()
 
-    @notifying
+    @notify('state_changed')
     def play_pause(self):
         self._paused = not self._paused
         self._mplayer.pause()
 
-    @notifying
-    def addToQueue(self, song, index=None):
+    @notify('queue_changed')
+    def add_to_queue(self, song, index=None):
         if index is None:
             self._queue.append(song)
         else:
             self._queue.insert(song, index)
 
+    @notify('queue_changed')
+    def remove_from_queue(self, index):
+        # removing by id is a bad idea, because songs can repeat in playlist, so we remove basing on index in queue
+        self._queue.remove(index)
+
     # ---------------- PROPERTIES ----------------
+    # TODO: also need to be notifying
+    @property
+    def is_playing(self):
+        return self._is_playing
 
     @property
     def volume(self):
         return self._vol
 
     @volume.setter
+    @notify('state_changed')
+
     def volume(self, value):
         self._mplayer.volume = value
         self._vol = value
@@ -240,6 +261,7 @@ class RPJPlayerV2(object):
         return self._mplayer.time_pos
 
     @time_pos.setter
+    @notify('state_changed')
     def time_pos(self, value):
         self._mplayer.time_pos = value
 
@@ -247,31 +269,45 @@ class RPJPlayerV2(object):
     def percent_pos(self):
         return self._mplayer.percent_pos
 
+    @percent_pos.setter
+    @notify('state_changed')
+    def percent_pos(self, value):
+        self._mplayer.percent_pos = value
+
     @time_pos.setter
+    @notify('state_changed')
     def time_pos(self, value):
         self._mplayer.percent_pos = value
 
-
     # ------------ EVENTS, HOOKS, etc ----------------
-    def on(self, event_name, handler):
-        if event_name in self._event_handlers:
-            self._event_handlers[event_name].append(handler)
+
+    def add_listener(self, event_name, listener):
+        self._event_listeners.append((event_name, listener))
+
+    def remove_listener(self, listener):
+        if listener in self._event_listeners:
+            self._event_listeners.pop(listener)
+        else:
+            raise LookupError('Listener not found')
+
+    # def mplayer_event_add(self, event_name, handler):
+    #    if event_name in self._mplayer_event_handlers:
+    #        self._mplayer_event_handlers[event_name].append(handler)
 
     def detach_handlers(self, event='all'):
         if event == 'all':
-            self._event_handlers["playback_finish"] = []
-            self._event_handlers["playback_skip"] = []
-            self._event_handlers["playback_stop"] = []
-        elif event in self._event_handlers:
-            self._event_handlers[event] = []
+            self._mplayer_event_handlers["playback_finish"] = []
+            self._mplayer_event_handlers["playback_skip"] = []
+            self._mplayer_event_handlers["playback_stop"] = []
+        elif event in self._mplayer_event_handlers:
+            self._mplayer_event_handlers[event] = []
 
-    def execute_event_hooks(self):
-        #executes all the function hooks asynchronously as greenlets
-        def pr():
-            print self.state()
-        eventlet.spawn(pr)
-        for hook in self._event_hooks:
-            eventlet.spawn(hook)
+    def fire_event(self, event_name):
+        # executes all the function listeners. Use @notifying decorator to fire asynchronously
+        state = self.state()
+        print state
+        for listener in filter(lambda l: l[0] == event_name, self._event_listeners):
+            listener(state)
 
     def _playback_end_event_handler(self, data):
         if data.startswith('EOF code: '):
@@ -280,42 +316,41 @@ class RPJPlayerV2(object):
             code = int(data[len('EOF code: '):])
             if code == 1:
                 # print 'RPJPlayer: playback finished'
-                handlers = self._event_handlers['playback_finish']
+                handlers = self._mplayer_event_handlers['playback_finish']
             elif code == 2:
                 # print 'RPJPlayer: playback skipped'
-                handlers = self._event_handlers['playback_skip']
+                handlers = self._mplayer_event_handlers['playback_skip']
             elif code == 4:
                 # print 'RPJPlayer: playback stopped'
-                handlers = self._event_handlers['playback_stop']
+                handlers = self._mplayer_event_handlers['playback_stop']
             else:
                 handlers = None
 
             if handlers:
                 for handler in handlers:
                     handler()
-            self.execute_event_hooks()
-            # print 'mplayer pipe: ' + data
-
-
-
+                    # print 'mplayer pipe: ' + data
+            eventlet.spawn(self.fire_event, 'state_changed')
 
 if __name__ == '__main__':
     # some test routines
-    import RPJqueue
-
     p = RPJPlayerV2(workingDirectory='songs')
     time.sleep(3)
     p.load('w9QfN-ODGWM.mp3')
     time.sleep(10)
-    p.addToQueue('w9QfN-ODGWM.mp3')
-    p.addToQueue('SYM-RJwSGQ8.mp3')
+    p.add_to_queue('w9QfN-ODGWM.mp3')
+    p.add_to_queue('SYM-RJwSGQ8.mp3')
     print p.volume
     p.load('SYM-RJwSGQ8.mp3')
-    time.sleep(10)
+    # time.sleep(10)
     print p.percent_pos
-    time.sleep(2)
+    time.sleep(5)
+    p.percent_pos = 0
+    while p.is_playing: time.sleep(2)
+
     # p.on('playback_finish', printeof)
     # p.play('w9QfN-ODGWM')
     # print p.percent_pos
 
     # p.quit()
+
